53d78c59404fe7f7a570adb8c94911e918396334
mohit <mohit.madan@gmail.com>
2015-05-06 08:47:25 +0530

    update

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-My First Docker Javascript 
+var express = require('express');
+
+// Constants
+var PORT = 8080;
+
+// App
+var app = express();
+app.get('/', function (req, res) {
+  res.send('Hello world\n');
+});
+
+app.listen(PORT);
+console.log('Running on http://localhost:' + PORT);
