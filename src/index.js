2ebf9fbaa6356b5d4cbd23d0947f5ad780da4d45
mohit <mohit.madan@gmail.com>
2015-05-23 19:22:46 +0530

    update the site

--- src/index.js
+++ src/index.js
@@ -1,16 +1,21 @@
 var express = require('express');
 var app = express();
-var path    = require("path");
+var path = require("path");
 
 app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'css')));
 
 // Constants
 var PORT = 8080;
 
 // App
 
-app.get('/', function (req, res) {
-  res.sendfile(path.join(__dirname+'/home.htm'));
+app.get('/', function(req, res) {
+	res.sendfile(path.join(__dirname + '/home.htm'));
+});
+
+app.get('/blog', function(req, res) {
+	res.redirect('http://www.mohitmadan.in:8888/')
 });
 
 app.listen(PORT);
